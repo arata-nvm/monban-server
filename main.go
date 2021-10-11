82cb1543a8ea265a0e23c0a08cda54d042ede010
arata <49086220+arata-nvm@users.noreply.github.com>
2021-10-11 15:35:48 +0000

    feat: add echo router

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	e := newRouter()
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func newRouter() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.POST("/", postEnter)
+
+	return e
+}
+
+func postEnter(ctx echo.Context) error {
+	return ctx.NoContent(http.StatusNoContent)
 }
